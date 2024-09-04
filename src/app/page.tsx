6c808328469658affbdbfd18bde702239104152c
Kako Ferrare <kakoferrare87@gmail.com>
2024-09-04 17:46:09 -0300

    feat: add state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,37 @@
+'use client'
 
 import UltimoResultado from "@/components/UltimoResultado";
 import { IJogo } from "@/utils/interface/models/jogo";
-import { GetJogos } from "@/utils/requests/probabilidade.http";
+import { ILoteria } from "@/utils/interface/models/loteria";
+import { GetJogos, GetUltimaLoteria } from "@/utils/requests/probabilidade.http";
+import { useState, useEffect } from "react";
 
 
-export default async function Home() {
-  const data: IJogo[] = await GetJogos();
+export default function Home() {
 
-  let titleSelect: string = "jogo";
-  let numbers: Array<string> = ["2", "3", "4"];
+  let [jogos, setJogos] = useState(new Array<IJogo>)
+  let [loteria, setLoteria] = useState<ILoteria>()
+
+  useEffect(() => {
+    GetJogos().then((res) => {
+      setJogos(res)
+    });;
+  }, []);
+
+
+  async function handleLoteria(event: React.ChangeEvent<any>): Promise<void> {
+    const ultimaLoteria: ILoteria = await GetUltimaLoteria(event.currentTarget.value);
+    setLoteria(ultimaLoteria)
+  }
 
   return (
     <main className="flex flex-col">
       <h1 className="text-4xl font-semibold dark:text-black">Probabilidade</h1>
-      <select id="jogos" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <select name="loteria" id="loteria" onChange={handleLoteria} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option defaultValue={0} disabled>Selecione o Jogo</option>
-        {data.map((item: any) => <option key={item.id} value={item.id}>{item.nome}</option>)}
+        {jogos.map((item: any) => <option key={item.id} value={item.id}>{item.nome}</option>)}
       </select>
-      <UltimoResultado title={titleSelect} numbers={numbers} />
+      <UltimoResultado title={loteria?.nome ?? ""} numbers={loteria?.dezenasOrdemSorteio ?? new Array<string>} />
     </main>
   );
 }
--- src/utils/requests/probabilidade.http.ts
+++ src/utils/requests/probabilidade.http.ts
@@ -2,11 +2,12 @@ import { IJogo } from "../interface/models/jogo";
 import { ILoteria } from "../interface/models/loteria";
 
 export async function GetJogos(): Promise<IJogo[]> {
-    const response = await fetch(`${process.env.URL_PROBABILIDADE_API}/jogo`);
+    const response = await fetch(`http://localhost:5133/jogo`);
+    //const response = await fetch(`${process.env.URL_PROBABILIDADE_API}/jogo`);
     return await response.json() as IJogo[];
 }
 
 export async function GetUltimaLoteria(loteria: string): Promise<ILoteria> {
-    const response = await fetch(`${process.env.URL_PROBABILIDADE_API}/loteria/${loteria}/ultima`);
+    const response = await fetch(`http://localhost:5133/loteria/${loteria}/ultima`);
     return await response.json() as ILoteria;
 }
